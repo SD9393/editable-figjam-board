b3263af9dc793e117de29c76d26d13940b71c1a3
Surya <suryacando@gmail.com>
2026-02-03 22:44:06 +0530

    Add real Firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
+  apiKey: "AIzaSyCknjodvT095K0M3ASxCEeeoChn9RBU1CQ",
+  authDomain: "realtime-board-ocd.firebaseapp.com",
   databaseURL: "https://realtime-board-ocd-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  projectId: "realtime-board-ocd",
+  storageBucket: "realtime-board-ocd.firebasestorage.app",
+  messagingSenderId: "258036246275",
+  appId: "1:258036246275:web:a93405647e4013aef3eb36",
 };
 
 const app = initializeApp(firebaseConfig);
